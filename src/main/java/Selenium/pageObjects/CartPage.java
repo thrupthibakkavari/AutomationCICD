91f4e6d35c7e8250911335071d90dc3a3f9fe304
thrupthibakkavari <thrupthibakkavari@gmail.com>
2024-05-30 22:18:21 +0530

    Update CartPage.java

--- src/main/java/Selenium/pageObjects/CartPage.java
+++ src/main/java/Selenium/pageObjects/CartPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
+
 
 import Selenium.AbstractComp.AbstractComponents;
 
